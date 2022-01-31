25e645a055f70b35322ac6f6367cb15d64030759
milosoria <camilo.soria@uc.cl>
2022-01-31 14:28:25 -0300

    Must restart searchIng

--- searchIng/src/scripts/handler.js
+++ searchIng/src/scripts/handler.js
@@ -1,5 +1,6 @@
 //import {searchBarUI} from "./searchBarUI.js";
-let handler = null; 
+
+let handler = null;
 
 class Handler {
 
@@ -7,115 +8,132 @@ class Handler {
         console.log("Intializing handler");
         chrome.runtime.onMessage.addListener(this.listen);
         this.dataPath = "../../data.json";
-        this.dataFiltered = {}
+        this.dataFiltered = {};
         this.executing = false;
-        this.searhBarUI = {}
+        this.searhBarUI = null;
 
         try {
             const url = chrome.runtime.getURL(this.dataPath);
-            console.log("URL",url);
+            console.log("URL", url);
             fetch(url)
-                .then((response) =>response.json())
-                .then((data)=>{
+                .then((response) => response.json())
+                .then((data) => {
                     this.searchData = data;
-                })
+                });
         } catch (error) {
-
             this.searchData = {};
             console.log("ERROR IN JSON PARSING", error);
-
         }
+        this.createIframe();
+    }
 
+    createIframe(){
+
+        const styleSheet = DomUtils.createElement("style");
+        styleSheet.type = "text/css";
+        // Default to everything hidden while the stylesheet loads.
+        styleSheet.innerHTML = "iframe {display: none;}";
+        this.iframeElement = document.createElement("iframe");
+        Object.assign(this.iframeElement, {
+            className,
+            seamless: "seamless"
+        });
+
+        const shadowWrapper = document.createElement("div");
+
+        if (shadowWrapper.attachShadow)
+            this.shadowDOM = shadowWrapper.attachShadow({mode: "open"});
+        else
+            this.shadowDOM = shadowWrapper;
+
+        this.shadowDOM.appendChild(styleSheet);
+        this.shadowDOM.appendChild(this.iframeElement);
+        this.iframeElement.classList.add("searchBarUIComponentHidden");
     }
 
     listen(request, sender, sendResponse) {
-        console.log("request:",request,"from:",sender);
+        console.log("request:", request, "from:", sender);
 
-        // if request init -> call search
-        if ('initialize' in request && !this.executing){
-            // this.sendMessage({received:true});
-            console.log("Url valid, starting to load search bar");
-            this.init();
-            sendResponse({ request: 'Initializing searchBarUI' });
+        switch(request){
+            // if request init -> call search
+            case "initialize" in request && !this.executing:
+                // this.sendMessage({received:true});
+                console.log("Url valid, starting to load search bar");
+                sendResponse({ request: "Initializing searchBarUI" });
+            
+
+            // if message unload -> call unload and clear dataFiltered
+            case "unload" in request:
+                this.unload();
+        }   
+    }
+
+    sendMessage(action, type, message) {
+        data.action = action;
+        data.type = type;
+        data.message = message;
+        chrome.runtime.sendMessage({
+            receiver: "handler",
+            data: data,
+            destination: "background",
+            function(response) {},
+        });
+    }
+
+    search(input) {
+        // this has to search on key stroke for link that match, showing at most 5 best matches results
+    }
+
+    prompt() {
+
+        this.iframeElement.classList.remove("searchBarUIComponentHidden");
+        this.iframeElement.classList.add("searchBarUIComponentVisible");
+        
+        if (!this.iframeElement.searchBarUI) {
+            // Add search Bar Ui to screen, prompt over the middle of the y axis
+            this.iframeElement.searchBarUI = this.iframeElement.createElement("div");
+            this.iframeElement.searchBarUI.setAttribute("class", "search_bar");
+            this.iframeElement.searchBarUI.addEventListener("onfocusout", function (_) {
+                // Close in case focus is lost
+                this.close();
+            });
+
+            // Add event listener for enter
+            this.iframeElement.searchBarUI.addEventListener(
+                "keypress",
+                function (e) {
+                    if (e.keyCode == 13) {
+                        // get which link was selected and redirect to it
+                        // sends message to background, so it redirects the page
+
+                        this.sendMessage("redirect to url", "redirect", "");
+                    } else if (e.keyCode == 27) {
+                        // escape key is pressed, then we close the prompt
+
+                        this.close();
+                    }
+                },
+                false
+            );
         }
-
-        // if message unload -> call unload and clear dataFiltered
-        if ('unload' in request){
-            this.unload();
-        }
     }
 
-    sendMessage(request) {
-        console.log("Sending the following request: ", request);
-        chrome.runtime.sendMessage(request, (response) => {});
-    }
-
-    init() {
-        // change this after unload is called
-        console.log("ISTHISHAPPENING");
-        this.executing = true;
-        let iframe = document.createElement('iframe');
-        iframe.setAttribute("src", "https://www.facebook.com/plugins/like.php?href=http://allofrgb.blogspot.in/");
-        iframe.setAttribute("style", "border:none; width:150px; height:30px");
-        iframe.setAttribute("scrolling", "no");
-        iframe.setAttribute("frameborder", "0");
-        document.body.appendChild(iframe);
-        console.log("APPENDING iframe to document", iframe);
-        // add event listener and call search
-        // TODO: closee
-    }
-
-    search(input){
-        // search in searchCache depending on the input, should this use regex or pure js
-        // if no key in this.dataName matches the input then send a message notifying the searchBarUI
-    }
-    prompt(){
-        // Add bubble to the top of the page.
-        this.searchBarUI = document.createElement('div');
-        this.searchBarUI.setAttribute('class', 'search_bar');
-        document.body.appendChild(this.searchBarUI);
-
-        // Lets listen to mouseup DOM events.
-        document.addEventListener('mouseup', function (e) {
-            var selection = window.getSelection().toString();
-            if (selection.length > 0) {
-                renderBubble(e.clientX, e.clientY, selection);
-            }
-        }, false);
-
-        // Close the bubble when we click on the screen.
-            document.addEventListener('mousedown', function (e) {
-                bubbleDOM.style.visibility = 'hidden';
-            }, false);
-
-        // Move that bubble to the appropriate location.
-            function renderBubble(mouseX, mouseY, selection) {
-                bubbleDOM.innerHTML = selection;
-                bubbleDOM.style.top = mouseY + 'px';
-                bubbleDOM.style.left = mouseX + 'px';
-                bubbleDOM.style.visibility = 'visible';
-            }
-    }
-    unload() {
+    close() {
         // unload searchCache
+        this.iframeElement.classList.add("searchBarUIComponentHidden");
         this.searchCache = {};
         this.executing = false;
     }
-
 }
 
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if(request.data.destination=="handler"){
-            if (request.data.action=="prompt_search_bar"){
-                if (!handler){
-                    handler = new Handler();
-                    handler.init(); 
-                } else {
-                    handler. 
-                }
+chrome.runtime.onMessage.addListener(function (request, _, _) {
+    console.log(request);
+    if (request.data.destination == "handler") {
+        if (request.data.action == "prompt_search_bar") {
+            if (!handler) {
+                handler = new Handler();
             }
+            handler.prompt();
         }
     }
-);
-
+});
